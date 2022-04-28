b09fbbddf648efd083db1dd7e41bdcd35b71b432
ToTo <99253733+ASTM1999@users.noreply.github.com>
2022-04-28 16:11:20 +0700

    Edit Code index.js & App.js

--- Tools/recoil/src/App.js
+++ Tools/recoil/src/App.js
@@ -1,9 +1,14 @@
 import './App.css'
+import { atom } from "recoil";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <h1>Hello React Recoil</h1>
+      <h2>The count Value is : </h2>
+      <h3>The font size of potty is : </h3>
+      <button>Increase count</button>
+      <button>Reset count</button>
     </div>
   );
 }
